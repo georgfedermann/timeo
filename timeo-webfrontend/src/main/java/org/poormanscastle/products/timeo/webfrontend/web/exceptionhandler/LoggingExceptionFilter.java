de7ad76a595df8084b86cf0ef5112dadb94672e4
Georg Federmann <georg.federmann@gmail.com>
2017-04-30 23:29:48 +0200

    Bug-fix services pom.configuration spring version; Setup timeo-webfrontend configuration

---
+++ timeo-webfrontend/src/main/java/org/poormanscastle/products/timeo/webfrontend/web/exceptionhandler/LoggingExceptionFilter.java
@@ -0,0 +1,42 @@
+package org.poormanscastle.products.timeo.webfrontend.web.exceptionhandler;
+
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+
+/**
+ * Created by georg on 30/04/2017.
+ */
+public class LoggingExceptionFilter implements Filter {
+
+    private final static Logger logger = Logger.getLogger(LoggingExceptionFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        ;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        try {
+            chain.doFilter(request, response);
+        } catch (Throwable exception) {
+            logger.error(StringUtils.join("An exception occurred within the Timeo Webfrontend: ", exception.getMessage()), exception);
+            throw exception;
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}

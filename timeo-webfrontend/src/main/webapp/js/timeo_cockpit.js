1f6b29252fb8e0899fca386f3b80422c522efbe8
Georg Federmann <georg.federmann@gmail.com>
2017-05-03 12:51:32 +0200

    Implement profile button context menu

--- timeo-webfrontend/src/main/webapp/js/timeo_cockpit.js
+++ timeo-webfrontend/src/main/webapp/js/timeo_cockpit.js
@@ -12,11 +12,11 @@ function ProfileButton() {
  */
 ProfileButton.prototype.init = function() {
     var me = this;
-    $("div#profile_button").mouseover(me.handleMouseover.bind(this));
-    $("div#profile_button img").mouseover(me.handleMouseover.bind(this));
-
-    $("div#profile_button").mouseout(me.handleMouseout.bind(this));
-    $("div#profile_button img").mouseout(me.handleMouseout.bind(this));
+    var profileButton = $("div#profile_button");
+    var image = $("div#profile_button img");
+    image.mouseover(me.handleMouseover.bind(this));
+    image.mouseout(me.handleMouseout.bind(this));
+    image.click(me.handleMouseclick.bind(this));
 }
 
 ProfileButton.prototype.handleMouseover = function() {
@@ -33,6 +33,20 @@ ProfileButton.prototype.handleMouseout = function() {
     image.removeAttr("style");
 }
 
+ProfileButton.prototype.handleMouseclick = function() {
+    console.log("Mouseclick on profile button.");
+    var image=$("div#profile_button img");
+    var contextMenu = $("div#profileContextMenu");
+    contextMenu.toggleClass("profileContextMenuHidden profileContextMenuVisible");
+    // contextMenu.attr("class", "profileContextMenuVisible");
+    var right = image.position().left + image.width();
+    var top = image.position().top + image.height();
+    var paddingSize = 8;
+    var left = right - contextMenu.width() - paddingSize;
+    contextMenu.css("top", top);
+    contextMenu.css("left", left);
+}
+
 /**
  * Implements functionality of the ReceiveCallButton
  */
@@ -43,11 +57,10 @@ function ReceiveCallButton() {
 
 ReceiveCallButton.prototype.init = function() {
     var me = this;
-    $("div#receive_call_button").mouseover(me.handleMouseover.bind(this));
-    $("div#receive_call_button img").mouseover(me.handleMouseover.bind(this));
-
-    $("div#receive_call_button").mouseout(me.handleMouseout.bind(this));
-    $("div#receive_call_button img").mouseout(me.handleMouseout.bind(this));
+    var receiveCallButton = $("div#receive_call_button");
+    var image = $("div#receive_call_button img"); 
+    image.mouseover(me.handleMouseover.bind(this));
+    image.mouseout(me.handleMouseout.bind(this));
 }
 
 ReceiveCallButton.prototype.handleMouseover = function() {

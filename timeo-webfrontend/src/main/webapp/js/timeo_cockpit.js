f380a7e4c90a9f602646a6303ba1c6dd65bd034c
Georg Federmann <georg.federmann@gmail.com>
2017-05-04 15:05:59 +0200

    Implement basic dropdown menu logic; Start integrating coverflow

--- timeo-webfrontend/src/main/webapp/js/timeo_cockpit.js
+++ timeo-webfrontend/src/main/webapp/js/timeo_cockpit.js
@@ -1,4 +1,18 @@
 /**
+ * on page load initiate diverse JavaScript objects to handle
+ * logic to be performed within the cockpit
+ */
+$(document).ready(function(){
+    console.log("cockpit loaded.");
+    profileButton = new ProfileButton();
+    profileButton.init();
+
+    receiveCallButton = new ReceiveCallButton();
+    receiveCallButton.init();
+
+});
+
+/**
  * Implements functionality of the ProfileButton
  * @constructor
  */
@@ -28,13 +42,17 @@ ProfileButton.prototype.handleMouseover = function() {
 
 ProfileButton.prototype.handleMouseout = function() {
     console.log("Mouseout profile button.");
-    var image = $("div#profile_button img");
-    image.attr("src", image.attr("src").replace("mouseover", this.status));
-    image.removeAttr("style");
+    if(this.status != "selected") {
+        var image = $("div#profile_button img");
+        image.attr("src", image.attr("src").replace("mouseover", this.status));
+        image.removeAttr("style");
+    }
 }
 
 ProfileButton.prototype.handleMouseclick = function() {
     console.log("Mouseclick on profile button.");
+    this.status = this.status == "active" ? "selected" : "active";
+    console.log("Switched ProfileButton status to " + this.status);
     var image=$("div#profile_button img");
     var contextMenu = $("div#profileContextMenu");
     contextMenu.toggleClass("profileContextMenuHidden profileContextMenuVisible");
@@ -61,6 +79,7 @@ ReceiveCallButton.prototype.init = function() {
     var image = $("div#receive_call_button img"); 
     image.mouseover(me.handleMouseover.bind(this));
     image.mouseout(me.handleMouseout.bind(this));
+    image.click(me.handleMouseclick.bind(this));
 }
 
 ReceiveCallButton.prototype.handleMouseover = function() {
@@ -72,7 +91,24 @@ ReceiveCallButton.prototype.handleMouseover = function() {
 
 ReceiveCallButton.prototype.handleMouseout = function() {
     console.log("Mouseout receive call button.");
-    var image = $("div#receive_call_button img");
-    image.attr("src", image.attr("src").replace("mouseover", this.status));
-    image.removeAttr("style");
+    if(this.status != "selected") {
+        var image = $("div#receive_call_button img");
+        image.attr("src", image.attr("src").replace("mouseover", this.status));
+        image.removeAttr("style");
+    }
 }
+
+ReceiveCallButton.prototype.handleMouseclick = function() {
+    console.log("Mouseclick registered on phone button.");
+    this.status = this.status == "active" ? "selected" : "active";
+    var image = $("div#receive_call_button img");
+    var phoneCallDialog = $("div#phoneCallDialog");
+    phoneCallDialog.toggleClass("phoneCallDialogHidden phoneCallDialogVisible");
+    var right = image.position().left + image.width();
+    var top = image.position().top + image.height();
+    var paddingSize = 8;
+    var left = right - phoneCallDialog.width() - paddingSize;
+    phoneCallDialog.css("top", top);
+    phoneCallDialog.css("left", left);
+}
+

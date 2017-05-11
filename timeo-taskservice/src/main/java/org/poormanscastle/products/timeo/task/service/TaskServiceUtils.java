2a8552dbb26083cc19bccf9e7dccb78b171fa499
Georg Federmann <georg.federmann@gmail.com>
2017-05-11 21:59:14 +0200

    Implement some basic logic to parse duration strings

--- timeo-taskservice/src/main/java/org/poormanscastle/products/timeo/task/service/TaskServiceUtils.java
+++ timeo-taskservice/src/main/java/org/poormanscastle/products/timeo/task/service/TaskServiceUtils.java
@@ -4,6 +4,8 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
@@ -17,12 +19,67 @@ public class TaskServiceUtils {
 
     final static Logger logger = Logger.getLogger(TaskServiceUtils.class);
 
+    /**
+     * Dates in the task service are formatted using this pattern:
+     * yyyy-MM-dd hh:mm:ss. Hours are given in 24 hours format. Months are numbered from
+     * 1 (January) through 12 (December). No time zone is given ... grand!
+     * TODO enable time zones!
+     */
     final static DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    /**
+     * durations - e.g. in effort estimations - are given by using a string like 4d meaning 4 days,
+     * or 20h, meaning 20 hours. If you should choose to combine different scales always proceed from
+     * the large scale to the lower, as in 1d 4h, not 4h 1d.
+     */
+    private final static Pattern durationParsePattern = Pattern.compile("(\\d+)d\\s+(\\d+)h\\s+(\\d+)m\\s+(\\d+)s");
+
+    private final static Pattern grepDaysPattern = Pattern.compile("(\\d+)d");
+    private final static Pattern grepHoursPattern = Pattern.compile("(\\d+)h");
+    private final static Pattern grepMinutesPattern = Pattern.compile("(\\d+)m");
+    private final static Pattern grepSecondsPattern = Pattern.compile("(\\d+)s");
+
+
+    public static int parseDurationString(String durationString) {
+        // TODO implement some logic to reject invalid duration strings
+        int numberOfSeconds = 0;
+        // extract days
+        Matcher matcher = grepDaysPattern.matcher(durationString);
+        while (matcher.find()) {
+            numberOfSeconds += 8 * 60 * 60 * Integer.parseInt(matcher.group(1));
+        }
+        // extract hours
+        matcher = grepHoursPattern.matcher(durationString);
+        while (matcher.find()) {
+            numberOfSeconds += 60 * 60 * Integer.parseInt(matcher.group(1));
+        }
+        // extract minutes
+        matcher = grepMinutesPattern.matcher(durationString);
+        while (matcher.find()) {
+            numberOfSeconds += 60 * Integer.parseInt(matcher.group(1));
+        }
+        // extract seconds
+        matcher = grepSecondsPattern.matcher(durationString);
+        while (matcher.find()) {
+            numberOfSeconds += Integer.parseInt(matcher.group(1));
+        }
+
+        return numberOfSeconds;
+    }
 
     public static String formatDate(Date date) {
         return TaskServiceUtils.dateFormat.format(date);
     }
 
+    /**
+     * Dates in the task service are formatted using this pattern:
+     * yyyy-MM-dd hh:mm:ss. Hours are given in 24 hours format. Months are numbered from
+     * 1 (January) through 12 (December). No time zone is given ... grand!
+     * TODO enable time zones!
+     *
+     * @param dateString
+     * @return
+     */
     public static Date parseDate(String dateString) {
         try {
             return TaskServiceUtils.dateFormat.parse(dateString);
---
+++ timeo-taskservice/src/test/java/org/poormanscastle/products/timeo/task/service/TaskServiceUtilsTest.java
@@ -0,0 +1,51 @@
+package org.poormanscastle.products.timeo.task.service;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by georg on 11/05/2017.
+ */
+public class TaskServiceUtilsTest {
+
+    TaskServiceUtils utils;
+
+    @Before
+    public void setup() {
+        utils = new TaskServiceUtils();
+    }
+
+    @Test
+    public void testParseDurationString1() throws Exception {
+        int result = utils.parseDurationString("4s 10s 6s");
+        assertEquals(20, result);
+    }
+
+    @Test
+    public void testParseDurationString2() throws Exception {
+        assertEquals(200, utils.parseDurationString("3m 20s"));
+    }
+
+    @Test
+    public void testParseDurationString3() throws Exception {
+        assertEquals(36600, utils.parseDurationString("10h10m"));
+    }
+
+    @Test
+    public void testParseDurationString4() throws Exception {
+        assertEquals(123048, utils.parseDurationString("4d2h10m48s"));
+    }
+
+    @Test
+    public void testParseDurationString5() throws Exception {
+        assertEquals(0, utils.parseDurationString("48"));
+    }
+
+    @Test
+    public void testParseDurationString6() throws Exception {
+        assertEquals(0, utils.parseDurationString("Poorman's Castle rules!"));
+    }
+
+}
